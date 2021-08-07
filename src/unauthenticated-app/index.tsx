eaf4b9e5d703cb693702e04a6b9db9e0e50b410e
Xi <xluo62@uwo.ca>
2021-08-08 01:02:37 +0800

    style: updated

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -2,38 +2,50 @@ import { useState } from "react";
 import { RegisterScreen } from "./register";
 import { LoginScreen } from "./login";
 import { Card } from "antd";
-import styled from '@emotion/styled';
-import logo from 'assets/logo.svg';
+import styled from "@emotion/styled";
+import logo from "assets/logo.svg";
+import left from "assets/logo.svg";
+import right from "assets/logo.svg";
 export const UnauthenticatedApp = () => {
   const [isRegistered, setIsRegistered] = useState(false);
 
-  return <Container>
-    <Header/>
-    <ShadowCard>
-      {isRegistered ? <RegisterScreen/> : <LoginScreen/>}
-      <button onClick={() => {setIsRegistered(!isRegistered)}}>切换到{isRegistered ? '登录' : '注册'}</button>
-    </ShadowCard>
-  </Container>
-}
+  return (
+    <Container>
+      <Header />
+      <ShadowCard>
+        {isRegistered ? <RegisterScreen /> : <LoginScreen />}
+        <button
+          onClick={() => {
+            setIsRegistered(!isRegistered);
+          }}
+        >
+          切换到{isRegistered ? "登录" : "注册"}
+        </button>
+      </ShadowCard>
+    </Container>
+  );
+};
 const Background = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
-`
+  background-attachment: fixed;
+  background-image: url(${left}), url(${right});
+`;
 
 const Header = styled.div`
   background: url(${logo}) no-repeat center;
   padding: 5rem 0;
   background-size: 8rem;
   width: 100%;
-`
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   min-height: 100vh;
-`
+`;
 
 const ShadowCard = styled(Card)`
   width: 40rem;
@@ -41,6 +53,6 @@ const ShadowCard = styled(Card)`
   padding: 3.2rem 4rem;
   border-radius: 0.3rem;
   box-sizing: border-box;
-  box-shadow: rgba(0,0,0,0.1) 0 0 10px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
   text-align: center;
-`
+`;
