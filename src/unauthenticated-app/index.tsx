815147ad3ea9f9b736e276b0f4d02f34c28e5c08
Xi <xluo62@uwo.ca>
2021-08-08 13:05:58 +0800

    style: updated the login screen styles

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
 import { RegisterScreen } from "./register";
 import { LoginScreen } from "./login";
-import { Card } from "antd";
+import { Button, Card } from "antd";
 import styled from "@emotion/styled";
 import logo from "assets/logo.svg";
-import left from "assets/logo.svg";
-import right from "assets/logo.svg";
+import left from "assets/left.svg";
+import right from "assets/right.svg";
 export const UnauthenticatedApp = () => {
   const [isRegistered, setIsRegistered] = useState(false);
   return (
     <Container>
       <Header />
+      <Background />
+
       <ShadowCard>
+        <Title>{isRegistered ? "请注册" : "请登录"}</Title>
         {isRegistered ? <RegisterScreen /> : <LoginScreen />}
-        <button
+        <LongButton
           onClick={() => {
             setIsRegistered(!isRegistered);
           }}
         >
           切换到{isRegistered ? "登录" : "注册"}
-        </button>
+        </LongButton>
       </ShadowCard>
     </Container>
   );
@@ -30,6 +33,10 @@ const Background = styled.div`
   height: 100%;
   background-attachment: fixed;
   background-image: url(${left}), url(${right});
+  background-position: left bottom, right bottom;
+  background-size: calc(((100vw - 40rem) / 2) - 3.2rem),
+    calc(((100vw - 40rem) / 2) - 3.2rem), cover;
+  background-repeat: no-repeat;
 `;
 
 const Header = styled.div`
@@ -37,6 +44,11 @@ const Header = styled.div`
   padding: 5rem 0;
   background-size: 8rem;
   width: 100%;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 2.4rem;
+  color: rgb(94, 108, 132);
 `;
 
 const Container = styled.div`
@@ -54,4 +66,8 @@ const ShadowCard = styled(Card)`
   box-sizing: border-box;
   box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
   text-align: center;
+`;
+//button在login界面和register界面都有被使用，所以这里暴露出去
+export const LongButton = styled(Button)`
+  width: 100%;
 `;
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "../context/auth-context";
-import { Form, Input, Button } from "antd";
+import { Form, Input } from "antd";
+import { LongButton } from "unauthenticated-app";
 
 export const LoginScreen = () => {
   const { login, user } = useAuth();
@@ -24,9 +25,9 @@ export const LoginScreen = () => {
         <Input placeholder={"密码"} type="password" id="password" />
       </Form.Item>
       <Form.Item>
-        <Button type="primary" htmlType={"submit"}>
+        <LongButton type="primary" htmlType={"submit"}>
           登录
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
